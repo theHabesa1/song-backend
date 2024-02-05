e88ceab8fc64904a0fd2fabe29e63dfa60d71f59
Yared Abera <90505588+theHabesa1@users.noreply.github.com>
2024-02-05 05:43:35 +0300

    Update route.js

--- server/route.js
+++ server/route.js
@@ -1,14 +1,10 @@
 import express from 'express';
-// import { getUsers, addUser, getUserById, editUser, deleteUser } from '../controller/user-controller.js';
+
 import { addSong, deleteSong, editSong, getSong, getSongById,} from '../controller/song-controller.js';
 
 const router = express.Router();
 
-// router.get('/', getUsers);
-// router.post('/add', addUser);
-// router.get('/:id', getUserById);
-// router.put('/:id', editUser);
-// router.delete('/:id', deleteUser);
+
 
 router.get('/', getSong);
 router.post('/add', addSong);
